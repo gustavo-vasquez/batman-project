3d6723eca088c0cb2070e4598d8750830911e46b
Gustavo <vasquez.gustavo90@gmail.com>
2016-01-03 00:18:28 -0300

    agregada funcionalidad para la busqueda (falta villanos)

---
+++ .bash_history
@@ -0,0 +1,4 @@
+git config user.name Gustavo
+git config user.email vasquez.gustavo90@gmail.com
+git status
+exit 
--- ProjectoLibre/Controllers/PracticaController.cs
+++ ProjectoLibre/Controllers/PracticaController.cs
@@ -1,4 +1,5 @@
-﻿using ProjectoLibre.Models;
+﻿using Newtonsoft.Json;
+using ProjectoLibre.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,18 +13,45 @@ namespace ProjectoLibre.Controllers
         //
         // GET: /Practica/
 
-        public JsonResult Sugerencias()
+        public JsonResult Sugerencias(string term)
         {
             BibliotecaDBEntities context = new BibliotecaDBEntities();
-            Busqueda busq = new Busqueda();
-            busq.nombres = context.Heroes.Select(h => h.nombre).ToList();
+            //Busqueda busq = new Busqueda();
+            //busq.nombres = context.Heroes.Select(h => h.nombre).ToList();
+            var result = (from r in context.Heroes
+                          where r.nombre.ToLower().Contains(term.ToLower())
+                          select new { r.nombre }).Distinct();
 
-            return Json(busq.nombres, JsonRequestBehavior.AllowGet);
+            return Json(result, JsonRequestBehavior.AllowGet);
+            //return JsonConvert.SerializeObject(busq.nombres);
+            //return busq.nombres.ToArray();
+        }
+
+        public string VerFecha()
+        {
+            return DateTime.Today.ToString();
         }
 
         public ActionResult VerJson()
         {
             return View();
+        }
+
+        public string WelcomeMsg(string input)
+        {
+            if (!String.IsNullOrEmpty(input))
+                return "Please welcome " + input + ".";
+            else
+                return "Please enter your name.";
+        }
+
+        public JsonResult ListaHeroe(int Id)
+        {
+            BibliotecaDBEntities db = new BibliotecaDBEntities();
+            var result = from r in db.Heroes
+                         where r.id == Id
+                         select new { r.nombre, r.habilidad };
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
